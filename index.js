cfcb46f03171e5d121b551851b15aba5596b4ec0
Barbara Gichohi <my.github.barbaragichohi77@gmail.com>
2022-08-17 21:45:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!';
+  }
+  else if (feet > 2000 && feet <= 2499) {
+    return 'I will gladly take your thirty bucks.';
+  }else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  if(city === 'NYC') {
+    return 'Ok, sounds good.';
+}else {
+  return 'No go.';
 }
+}
+  
+
+
+function switchOnCharmFromTip(say){
+  if (say ==='generous') {
+    return 'Thank you so much.';
+  }else if (say ==='not as generous') {
+    return 'Thank you.'; 
+  }else {
+    return 'Bye.';
+  }
+  }
